e6ca2687d0cf82873cf6564bc1a62d08464f9711
Imran Razak <Imranrazak1@icloud.com>
2022-01-04 16:42:14 +0000

    Initial Commit

---
+++ Mankind/MankindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MankindApp.swift
+//  Mankind
+//
+//  Created by Imran razak on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MankindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
